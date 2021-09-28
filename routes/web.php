7e637050e5110bda03cce02502571120d05230fa
BENHVIENCONGNGH\Minh Yen <phungyen98act@gmail.com>
2021-09-28 11:45:13 +0700

    laravel jetstream

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+Route::get('/billing', function () {
+    // ...
+})->middleware(['password.confirm']);
+
+Route::post('/billing', function () {
+    // ...
+})->middleware(['password.confirm']);
